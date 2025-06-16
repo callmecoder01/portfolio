6afc83cfb611ba32de1730a38f06c34a7dda7b0d
Saroj <geeksaroj@gmail.com>
2025-06-16 09:42:08 +0545

    change next config

--- next.config.js
+++ next.config.js
@@ -1,21 +1,16 @@
 /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: "https",
-//         hostname: "images.unsplash.com",
-//       },
-//     ],
-//   },
-//   experimental: {
-//     serverActions: true, // This is for experimental features in Next.js
-//   },
-//   // output: "export",
-//   exportTrailingSlash: true,
-// };
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
+  experimental: {
+    serverActions: true,
+  },
+};
 
-// module.exports = nextConfig;
-
-const nextConfig = { output: "export" };
 module.exports = nextConfig;
